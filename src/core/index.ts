8d2a2eb94330e94e8b72b4fbf20c123bf890ca51
artem <art_nazarov@mail.ru>
2024-08-26 02:15:45 +0300

    formatted code

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,5 +4,5 @@ export interface IProtocolService {
 	create(message: Message, username: string);
 	delete(message: Message, username: string);
 	getFile(message: Message, username: string);
-    getAll(message: Message);
+	getAll(message: Message);
 }
